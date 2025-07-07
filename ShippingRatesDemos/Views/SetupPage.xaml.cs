63ac6217949bb8e5c32d6cf8af2a2eee2aae2780
Alexis Maximiliano Costas <alexismcostas@gmail.com>
2025-07-07 14:04:00 -0300

    Implemented the UI of setupPage

--- ShippingRatesDemos/Views/SetupPage.xaml.cs
+++ ShippingRatesDemos/Views/SetupPage.xaml.cs
@@ -6,4 +6,17 @@ public partial class SetupPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void SfSegmentedControl_SelectionChanged(object sender,
+            Syncfusion.Maui.Toolkit.SegmentedControl.SelectionChangedEventArgs e)
+    {
+        Application.Current.UserAppTheme = e.NewIndex == 0
+            ? AppTheme.Light : AppTheme.Dark;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ThemeSegmentedControl.SelectedIndex =
+            Application.Current.UserAppTheme == AppTheme.Light ? 0 : 1;
+    }
 }
